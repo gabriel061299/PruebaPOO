e28fd616fc3c1332e070dc797285c1a3395edbf4
a2095833@correo.uia.mx <a2095833@correo.uia.mx>
2020-11-05 16:20:11 -0600

    Vista proyecto final

---
+++ Proyecto Final/src/com/gcb/cdc/vista/Vista.java
@@ -0,0 +1,23 @@
+package com.gcb.cdc.vista;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JComboBox;
+public class Vista extends JFrame{
+	
+	public  JButton Hombre;
+	public  JButton Mujer;
+	public  JTextArea persons;
+	public JComboBox ventnom;
+
+}
