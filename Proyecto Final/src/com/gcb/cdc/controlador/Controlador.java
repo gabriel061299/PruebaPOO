881aa7b6cb67486a77b6db4fbbe284842b34d1e2
a2095833@correo.uia.mx <a2095833@correo.uia.mx>
2020-11-09 20:29:44 -0600

    Controlador

--- Proyecto Final/src/com/gcb/cdc/controlador/Controlador.java
+++ Proyecto Final/src/com/gcb/cdc/controlador/Controlador.java
@@ -2,13 +2,24 @@ package com.gcb.cdc.controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+
+
 import com.gcb.cdc.modelo.ConectaTabla;
+import com.gcb.cdc.modelo.ModeloCovid;
 import com.gcb.cdc.vista.Vista;
 
 public class Controlador implements ActionListener{
 
 	Vista vistcod = new Vista();
-	ConectaTabla contabla = new ConectaTabla(); 
+	ConectaTabla contabla = new ConectaTabla();
+	int datos1 = 0;
+	int datos2 = 0;
+	int datos3 = 0;
+	ArrayList<ModeloCovid> idEntidades = new ArrayList<ModeloCovid>();
+	ArrayList<ModeloCovid> Genero = new ArrayList<ModeloCovid>();
+	
 	
 	public Controlador(Vista vistcod, ConectaTabla contabla){
 		
@@ -25,12 +36,14 @@ public class Controlador implements ActionListener{
 		this.vistcod.obesidad.addActionListener(this);	
 	}
 	
-	public void actionPerformed(ActionEvent e) {
+public void actionPerformed(ActionEvent e) {
 		
+		//vistcod.genero.clearSelection();	
+	
 		if(e.getSource() == vistcod.estados){
 			
 			vistcod.ventestados.removeAllItems();
-			
+			vistcod.ventestados.addItem("");
 			vistcod.ventestados.addItem("Aguascalientes");
 			vistcod.ventestados.addItem("Baja California");
 			vistcod.ventestados.addItem("Baja California Sur");
@@ -64,6 +77,7 @@ public class Controlador implements ActionListener{
 			vistcod.ventestados.addItem("Yucatán");
 			vistcod.ventestados.addItem("Zacatecas");
 			
+			
 			vistcod.ventestados.updateUI();
 			vistcod.ventestados.repaint();
 			
@@ -72,224 +86,237 @@ public class Controlador implements ActionListener{
 			
 			int num = vistcod.ventestados.getSelectedIndex();
 			if(num>=0){
-				if(num == 0){
+				if(num == 1){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
-					vistcod.info.setText("Aguas Calientes");
+					vistcod.info.setText("Aguascalientes");
+					for(int i=0;i<200;i++){
+						if(contabla.listacovid().get(i).getEntidad()== 1){
+							idEntidades.add(contabla.listacovid().get(i));
+						}
+					}
+					
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
-				if(num == 1){
+				if(num == 2){
 				//vistcod.info.selectAll();
 				//vistcod.info.replaceSelection("");
 				vistcod.info.setText("Baja California");
 				vistcod.info.updateUI();
 				vistcod.info.repaint();
 				}
-				if(num == 2){
+				if(num == 3){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
 					vistcod.info.setText("Baja California Sur");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
-				if(num == 3){
+				if(num == 4){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
 					vistcod.info.setText("Campeche");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
-				if(num == 4){
+				if(num == 5){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
 					vistcod.info.setText("Coahuila de Zaragoza");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
-				if(num == 5){
+				if(num == 6){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
 					vistcod.info.setText("Colima");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
-				if(num == 6){
+				if(num == 7){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
 					vistcod.info.setText("Chiapas");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
-				if(num == 7){
+				if(num == 8){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
 					vistcod.info.setText("Chihuahua");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
-				if(num == 8){
-					//vistcod.info.selectAll();
-					//vistcod.info.replaceSelection("");
-					vistcod.info.setText("Ciudad de México");
-					vistcod.info.updateUI();
-					vistcod.info.repaint();
-				}
 				if(num == 9){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
-					vistcod.info.setText("Durango");
+					vistcod.info.setText("Ciudad de México");
+					for(int i=0;i<25;i++){
+						if(contabla.listacovid().get(i).getEntidad()== 9){
+							idEntidades.add(contabla.listacovid().get(i));
+							datos1++;
+						}
+					}
+					System.out.println("Datos ent:"+ datos1);
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
 				if(num == 10){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
-					vistcod.info.setText("Guanajuato");
+					vistcod.info.setText("Durango");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
 				if(num == 11){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
-					vistcod.info.setText("Guerrero");
+					vistcod.info.setText("Guanajuato");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
 				if(num == 12){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
-					vistcod.info.setText("Hidalgo");
+					vistcod.info.setText("Guerrero");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
 				if(num == 13){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
-					vistcod.info.setText("Jalisco");
+					vistcod.info.setText("Hidalgo");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
 				if(num == 14){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
-					vistcod.info.setText("Estado de México");
+					vistcod.info.setText("Jalisco");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
 				if(num == 15){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
-					vistcod.info.setText("Michoacán");
+					vistcod.info.setText("Estado de México");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
 				if(num == 16){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
-					vistcod.info.setText("Morelos");
+					vistcod.info.setText("Michoacán");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
 				if(num == 17){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
-					vistcod.info.setText("Nayarit");
+					vistcod.info.setText("Morelos");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
 				if(num == 18){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
-					vistcod.info.setText("Nuevo León");
+					vistcod.info.setText("Nayarit");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
 				if(num == 19){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
-					vistcod.info.setText("Oaxaca");
+					vistcod.info.setText("Nuevo León");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
 				if(num == 20){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
-					vistcod.info.setText("Puebla");
+					vistcod.info.setText("Oaxaca");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
 				if(num == 21){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
-					vistcod.info.setText("Queretaro");
+					vistcod.info.setText("Puebla");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
 				if(num == 22){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
-					vistcod.info.setText("Quintana Roo");
+					vistcod.info.setText("Queretaro");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
 				if(num == 23){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
-					vistcod.info.setText("San Luis Potosi");
+					vistcod.info.setText("Quintana Roo");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
 				if(num == 24){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
-					vistcod.info.setText("Sinaloa");
+					vistcod.info.setText("San Luis Potosi");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
 				if(num == 25){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
-					vistcod.info.setText("Sonora");
+					vistcod.info.setText("Sinaloa");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
 				if(num == 26){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
-					vistcod.info.setText("Tabasco");
+					vistcod.info.setText("Sonora");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
 				if(num == 27){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
-					vistcod.info.setText("Tamaulipas");
+					vistcod.info.setText("Tabasco");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
 				if(num == 28){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
-					vistcod.info.setText("Tlaxcala");
+					vistcod.info.setText("Tamaulipas");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
 				if(num == 29){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
-					vistcod.info.setText("Veracruz");
+					vistcod.info.setText("Tlaxcala");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
 				if(num == 30){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
-					vistcod.info.setText("Yucatán");
+					vistcod.info.setText("Veracruz");
 					vistcod.info.updateUI();
 					vistcod.info.repaint();
 				}
 				if(num == 31){
+					//vistcod.info.selectAll();
+					//vistcod.info.replaceSelection("");
+					vistcod.info.setText("Yucatán");
+					vistcod.info.updateUI();
+					vistcod.info.repaint();
+				}
+				if(num == 32){
 					//vistcod.info.selectAll();
 					//vistcod.info.replaceSelection("");
 					vistcod.info.setText("Zacatecas");
@@ -300,47 +327,90 @@ public class Controlador implements ActionListener{
 		}
 		if (e.getSource() == vistcod.hombre){
 			
-			vistcod.info2.setText("Hombres");
+			vistcod.info2.setText("Población de Hombres");
+			for(int i=0;i<idEntidades.size();i++){
+				if(idEntidades.get(i).getSexo()== 2){
+					Genero.add(idEntidades.get(i));
+					datos2++;
+				}
+			}
+			System.out.println("Hombres:"+datos2);
 			vistcod.info2.updateUI();
 			vistcod.info2.repaint();
 		}
 		if (e.getSource() == vistcod.mujer){
 			
-			vistcod.info2.setText("Mujeres");
+			vistcod.info2.setText("Población de Mujeres");
+			for(int i=0;i<idEntidades.size();i++){
+				if(idEntidades.get(i).getSexo()== 1){
+					Genero.add(idEntidades.get(i));
+					datos2++;
+				}
+			}
+			System.out.println("Mujeres"+datos2);
 			vistcod.info2.updateUI();
 			vistcod.info2.repaint();
 		}
 		if (e.getSource() == vistcod.epoc){
 			
-			vistcod.info3.setText("EPOC");
+			vistcod.info3.setText("Número de Casos con EPOC");
+			datos3=0;
+			for(int i=0;i<Genero.size();i++){
+				if(Genero.get(i).getEpoc()== 1){
+					datos3++;
+				}
+			}
+			System.out.println(datos3);
 			vistcod.info3.updateUI();
 			vistcod.info3.repaint();
 		}
 		if (e.getSource() == vistcod.asma){
 			
-			vistcod.info3.setText("Asma");
+			vistcod.info3.setText("Número de Casos con Asma");
+			datos3=0;
+			for(int i=0;i<Genero.size();i++){
+				if(Genero.get(i).getAsma()== 1){
+					datos3++;
+				}
+			}
+			System.out.println(datos3);
 			vistcod.info3.updateUI();
 			vistcod.info3.repaint();
 		}
 		if (e.getSource() == vistcod.hipertension){
 			
-			vistcod.info3.setText("Hipertension");
+			vistcod.info3.setText("Número de Casos con Hipertension");
+			datos3=0;
+			for(int i=0;i<Genero.size();i++){
+				if(Genero.get(i).getHipertension()== 1){
+					datos3++;
+				}
+			}
+			System.out.println(datos3);
 			vistcod.info3.updateUI();
 			vistcod.info3.repaint();
 		}
 		if (e.getSource() == vistcod.obesidad){
 		
-			vistcod.info3.setText("Obesidad");
+			vistcod.info3.setText("Número de Casos con Obesidad");
+			datos3=0;
+			for(int i=0;i<Genero.size();i++){
+				if(Genero.get(i).getObesidad()== 1){
+					datos3++;
+				}
+			}
+			System.out.println(datos3);
 			vistcod.info3.updateUI();
 			vistcod.info3.repaint();
 		}
 		if (e.getSource() == vistcod.edad){
 			
-			vistcod.info3.setText("Edad");
+			vistcod.info3.setText("Rango de Edades");
 			vistcod.info3.updateUI();
 			vistcod.info3.repaint();
 		}
-		
+
+			
 	}
 
 }
